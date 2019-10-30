a9937e629fa337b17f77e9cd66892b1ee3d34c08
Skyler Parr <skylerparr@gmail.com>
2019-10-30 01:07:11 -0400

    dangling commit

--- src/StaticInit.hx
+++ src/StaticInit.hx
@@ -2,4 +2,17 @@ package ;
 
 class StaticInit {
 
+  public function new() {
+  }
+
+  public static function main(): Void {
+    StaticInit.print("hello world");
+    new StaticInit();
+  }
+
+  public static function print(str): Void {
+    python.Lib.print(str);
+  }
+
+
 }
